1fa7a9d613ea57e56c5ee6cf29a0c9cac345b937
Jan Kašpar <jan.kaspar@cern.ch>
2020-01-13 18:28:33 +0100

    Loop over different form factors.

--- test_cni_elegent.cc
+++ test_cni_elegent.cc
@@ -19,27 +19,35 @@ void PlotOne(const string &formula)
 	TGraph *g_dsdt = new TGraph(); g_dsdt->SetName("g_dsdt");
 	TGraph *g_phase = new TGraph(); g_phase->SetName("g_phase");
 
-	for (double mt = 1E-4; mt <= 0.2;)
+	const double t_min = 1E-4;
+	const double t_max = 1.5;
+	const int n = 300;
+
+	const double xi = pow(t_max/t_min, 1./(n-1));
+
+	double mt = t_min;
+	for (unsigned int i = 0; i < n; i++, mt *= xi)
 	{
 		TComplex A = 0;
 		
+		if (formula == "coulomb")
+			A = coulomb->Amp_pure(-mt);
+
 		if (formula == "trivial")
 			A = model->Amp(-mt) + coulomb->Amp_pure(-mt);
+
+		if (formula == "SWY")
+			A = coulomb->Amp_SWY(-mt);
 		
+		if (formula == "Cahn")
+			A = coulomb->Amp_Cahn(-mt);
+
 		if (formula == "KL")
 			A = coulomb->Amp_KL(-mt);
 
 		int idx = g_dsdt->GetN();
 		g_dsdt->SetPoint(idx, mt, cnts->sig_fac * A.Rho2());
 		g_phase->SetPoint(idx, mt, A.Theta());
-
-		double dmt = 1E-4;
-		if (mt > 3E-3)
-			dmt = 1E-3;
-		if (mt > 0.1)
-			dmt = 1E-2;
-
-		mt += dmt;
 	}
 
 	g_dsdt->Write();
@@ -64,8 +72,16 @@ int main()
 	};
 
 	vector<string> formulae = {
+		"coulomb",
 		"trivial",
+		"SWY",
+		"Cahn",
 		"KL",
+	};
+
+	vector<CoulombInterference::FFType> formFactors = {
+		CoulombInterference::ffNone,
+		CoulombInterference::ffPuckett,
 	};
 
 	// prepare output
@@ -80,9 +96,19 @@ int main()
 		for (const auto formula : formulae)
 		{
 			TDirectory *d_formula = d_model->mkdir(formula.c_str());
-			gDirectory = d_formula;
 
-			PlotOne(formula);
+			for (const auto &ff: formFactors)
+			{
+				if (formula != "coulomb" && ff == CoulombInterference::ffNone)
+					continue;
+
+				coulomb->ffType = ff;
+
+				TDirectory *d_ff = d_formula->mkdir(coulomb->GetFFName().c_str());
+				gDirectory = d_ff;
+
+				PlotOne(formula);
+			}
 		}
 	}
 
